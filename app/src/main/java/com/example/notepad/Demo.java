227a13b06f7fb7febf17d3f5222267beddffad05
GusionTan <you@example.com>
2018-12-18 08:55:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/notepad/Demo.java
@@ -0,0 +1,35 @@
+package com.example.notepad;
+
+public class Demo {
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String title;
+
+    public String getWords() {
+        return words;
+    }
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+
+    private String words;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    private String date;
+}
